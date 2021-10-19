d60ca8005698af4b8f700811d83fd08e67562aae
wangyahui <wangyahui@weihaizixun.com>
2021-10-19 15:11:27 +0800

    脱敏使用正则来处理日志中部分是json的情况

--- log/src/main/java/com/scindapsus/log/desensitize/LogDesensitizationConverter.java
+++ log/src/main/java/com/scindapsus/log/desensitize/LogDesensitizationConverter.java
@@ -7,15 +7,18 @@ import cn.hutool.core.util.DesensitizedUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.JSONValidator;
 import com.alibaba.fastjson.parser.Feature;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * 敏感信息数据转换器(目前仅支持整行日志是json格式的情况脱敏)
+ * 敏感信息数据转换器,目前仅支持json格式的脱敏
+ * <p>手动脱敏可直接使用hutool的工具类{@link cn.hutool.core.util.DesensitizedUtil}
  *
  * @author wyh
  * @date 2020年12月23日 16:59:25
@@ -25,12 +28,12 @@ public class LogDesensitizationConverter extends MessageConverter {
     /**
      * 日志脱敏开关
      */
-    private String allowRun;
+    private String allowRun = null;
 
     /**
      * 日志脱敏关键字
      */
-    private List<DesensitizeRule> desensitizeRules;
+    private List<DesensitizeRule> desensitizeRules = null;
 
 
     private static final String ENABLED_KEY = "DesensitizeEnabled";
@@ -40,7 +43,7 @@ public class LogDesensitizationConverter extends MessageConverter {
 
     @Override
     public String convert(ILoggingEvent event) {
-        // 从xml中获取定义的开关与脱敏关键字
+        //从xml中获取定义的开关与脱敏关键字
         if (allowRun == null) {
             Context context = getContext();
             String isEnabled = context.getProperty(ENABLED_KEY);
@@ -52,40 +55,62 @@ public class LogDesensitizationConverter extends MessageConverter {
             }
         }
 
-        // 获取原始日志
+        //获取原始日志
         String originalMessage = event.getFormattedMessage();
 
-        // 缺少必要参数不脱敏
+        //缺少必要参数不脱敏
         if (!ENABLED.equals(allowRun) || CollectionUtils.isEmpty(desensitizeRules) || !StringUtils.hasText(originalMessage)) {
             return originalMessage;
         }
 
-        // 日志脱敏
-        return filterMessage(originalMessage);
+        //日志脱敏
+        return Optional.ofNullable(regexFilterMessage(originalMessage, desensitizeRules))
+                .orElse(filterMessage(originalMessage, desensitizeRules));
     }
 
     /**
-     * 处理日志字符串，返回脱敏后的字符串
+     * 查找日志中的json串后脱敏
      *
-     * @param message 原始日志字符串
+     * @param message          原始日志
+     * @param desensitizeRules 脱敏规则
+     */
+    private static String regexFilterMessage(String message, List<DesensitizeRule> desensitizeRules) {
+        try {
+            String regEx = "(?=(\\[|\\{)).*([\\\\\\]$]|[\\}$])";
+            Pattern pat = Pattern.compile(regEx);
+            Matcher mat = pat.matcher(message);
+            if (mat.find()) {
+                return mat.replaceAll(filterMessage(mat.group(), desensitizeRules));
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 如果message是json则脱敏
+     *
+     * @param message          原始日志字符串
+     * @param desensitizeRules 脱敏规则
      * @return 脱敏后的字符串
      */
-    public String filterMessage(String message) {
-        JSONValidator.Type type = JSONValidator.from(message).getType();
-        if (type == null) {
+    private static String filterMessage(String message, List<DesensitizeRule> desensitizeRules) {
+        Object parse;
+        try {
+            parse = JSON.parse(message);
+        } catch (Exception e) {
             //如果不是json返回原始字符串
             return message;
         }
+
         JSON json;
-        switch (type) {
-            case Array:
-                json = JSON.parseArray(message);
-                break;
-            case Object:
-                json = JSON.parseObject(message, Feature.SortFeidFastMatch);
-                break;
-            default:
-                return message;
+        if (parse instanceof JSONArray) {
+            json = JSON.parseArray(message);
+        } else if (parse instanceof JSONObject) {
+            json = JSON.parseObject(message, Feature.SortFeidFastMatch);
+        } else {
+            return message;
         }
 
         //开始脱敏
