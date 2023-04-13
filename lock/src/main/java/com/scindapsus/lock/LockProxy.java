9dd39cb76bf6f5d8dc0f645287705122245e97b7
wangyahui <wangyahui@weihaizixun.com>
2023-04-13 15:29:14 +0800

    :zap:动态代理加锁优化

--- lock/src/main/java/com/scindapsus/lock/LockProxy.java
+++ lock/src/main/java/com/scindapsus/lock/LockProxy.java
@@ -4,12 +4,13 @@ import com.scindapsus.lock.exception.DistributedLockException;
 import com.scindapsus.lock.support.SpringExpressionLangParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.lang.Nullable;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.time.Duration;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 
 /**
@@ -26,22 +27,21 @@ public class LockProxy implements InvocationHandler {
 
     private final String express;
 
-    private final Object orElse;
+    private final Duration retryDuration;
 
-    public LockProxy(Object target, String express, Object orElse) {
+
+    public LockProxy(Object target, String express, Duration retryDuration) {
         this.target = target;
         this.express = express;
-        this.orElse = orElse;
+        this.retryDuration = retryDuration;
     }
 
 
     /**
      * jdk动态代理方法加锁，只适用于类实现了interface的方法的代理
-     * <p>
-     * 加锁失败抛{@link com.scindapsus.lock.exception.DistributedLockException}
      *
      * @param target  实现类
-     * @param express 琐key表达式（支持spring expression lang）
+     * @param express 琐key表达式（支持spel）
      * @param <T>     实现类类型
      * @return 代理后的类
      */
@@ -52,21 +52,21 @@ public class LockProxy implements InvocationHandler {
     /**
      * jdk动态代理方法加锁，只适用于类实现了interface的方法的代理
      *
-     * @param target  实现类
-     * @param express 琐key表达式（支持spring expression lang）
-     * @param orElse  如果加锁失败返回什么结果,为null则抛错{@link com.scindapsus.lock.exception.DistributedLockException}
-     * @param <T>     实现类类型
+     * @param target        实现类
+     * @param express       琐key表达式（支持spel）
+     * @param retryDuration 尝试获取琐多少时间,获取不到抛{@link DistributedLockException}
+     * @param <T>           实现类类型
      * @return 代理后的类
      */
     @SuppressWarnings("unchecked")
-    public static <T> T proxy(T target, String express, @Nullable Object orElse) {
+    public static <T> T proxy(T target, String express, Duration retryDuration) {
         if (target == null || express == null) {
             return target;
         }
         return (T) Proxy.newProxyInstance(
                 target.getClass().getClassLoader(),
                 target.getClass().getInterfaces(),
-                new LockProxy(target, express, orElse));
+                new LockProxy(target, express, retryDuration));
     }
 
     @Override
@@ -75,9 +75,30 @@ public class LockProxy implements InvocationHandler {
         String key = SpringExpressionLangParser.parse(target, express, method, args);
         //获取琐对象
         Lock lock = LockRegistryFactoryHolder.getLock().obtain(key);
+        return Optional.ofNullable(retryDuration)
+                .map(r -> tryLock(lock, method, args, r))
+                .orElseGet(() -> lock(lock, method, args));
+    }
+
+    private Object lock(Lock lock, Method method, Object[] args) {
+        //阻塞式获取琐
+        lock.lock();
+        try {
+            log.debug("---get lock succeed");
+            return method.invoke(target, args);
+        } catch (Exception e) {
+            log.error("try lock exception ->", e);
+            throw new DistributedLockException("get lock failed");
+        } finally {
+            lock.unlock();
+            log.debug("---release lock succeed");
+        }
+    }
+
+    private Object tryLock(Lock lock, Method method, Object[] args, Duration retryDuration) {
         boolean got = false;
         try {
-            if (got = lock.tryLock()) {
+            if (got = lock.tryLock(retryDuration.toMillis(), TimeUnit.MILLISECONDS)) {
                 log.debug("---try lock succeed");
                 return method.invoke(target, args);
             }
@@ -89,7 +110,6 @@ public class LockProxy implements InvocationHandler {
                 log.debug("---release lock succeed");
             }
         }
-        return Optional.ofNullable(orElse)
-                .orElseThrow(() -> new DistributedLockException("try lock failed"));
+        throw new DistributedLockException("get lock failed");
     }
 }
